4daf83254c6eee64b9a74e1afbdf6f75500505a1
Aleks Petkov <aleksp@kth.se>
2022-01-28 18:09:19 +0100

    #5 PUM matrix constructed and tested

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -47,4 +47,33 @@ class DecideTest {
         decide.params.FUV[0] = false; 
         assertFalse(decide.launch(decide.params.FUV));
     }
+
+    @Test
+    void test_generate_pum_elements() { 
+        Decide decide = new Decide();
+        decide.params.LCM[0][1] = Parameters.Connectors.ANDD;
+        decide.params.LCM[1][2] = Parameters.Connectors.ANDD;
+
+        decide.params.LCM[0][2] = Parameters.Connectors.ORR;
+        decide.params.LCM[0][3] = Parameters.Connectors.ORR;
+
+        decide.params.LCM[2][3] = Parameters.Connectors.NOTUSED;
+
+
+        decide.params.CMV[0] = false;
+        decide.params.CMV[1] = true;
+        decide.params.CMV[2] = true;
+        decide.params.CMV[3] = false;
+        decide.generatePUM();
+        // false ANDD true == false
+        assertFalse(decide.params.PUM[0][1]);
+        // true ANDD true == true
+        assertTrue(decide.params.PUM[1][2]);
+        // false ORR true == true
+        assertTrue(decide.params.PUM[0][2]);
+        // false ORR false == false
+        assertFalse(decide.params.PUM[0][3]);
+        // _ NOTUSED _ == true
+        assertTrue(decide.params.PUM[2][3]);
+    }
 }
